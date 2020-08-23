6ec1668c5572989353c7b4c1a1099296f5e1979b
ibourn <36422029+ibourn@users.noreply.github.com>
2020-08-23 23:16:10 +0200

    truffle init and erc20 contracts

---
+++ Week7/D4_truffle/contracts/erc20.sol
@@ -0,0 +1,60 @@
+import "./Ownable.sol";
+import "./Safemath.sol";
+pragma solidity 0.5.12;
+
+contract ERC20 is Ownable {
+
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = 18;
+    }
+
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSuppply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        require(account != address(0), "mint to the 0 address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "transfer to the 0 address");
+        require(_balances[msg.sender] >= amount, "insufficient balance");
+
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+
+        return true;
+    }
+}
